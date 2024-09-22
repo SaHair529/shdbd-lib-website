9a6ad538a8d98eb8f53d8d3e4513159242008873
SaHair529 <sa.hairulaev@gmail.com>
2024-09-22 03:11:30 +0300

    -Исправил урл к апи. -Убрал reportWebVitals(console.log) -Добавил начальное состояние ListPage

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from "../../apiConfig";
 
 function ListPage() {
+    const [books, setBooks] = useState([])
 
+    useEffect(() => {
+        api.get('/books')
+            .then(response => setBooks(response.data))
+            .catch(error => console.error('Error fetching books:', error))
+    }, []);
+
+    return (
+        <ul>
+            { books.map(book => (
+                <li key={book.id}>{book.title}</li>
+            )) }
+        </ul>
+    )
 }
 
 export default ListPage
